4d59f6723181326f2527cca155531141ddc4a4cc
Anderson Bargas <anderson@andersonbargas.com>
2020-09-23 19:20:47 -0300

    Fixes the ISO filename.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # download iso to 9front.iso
-curl -L http://9front.org/iso/9front-7781.38dcaeaa222c.amd64.iso.gz -o plan9.iso
+curl -L http://9front.org/iso/9front-7781.38dcaeaa222c.amd64.iso.gz -o 9front.iso
 
 # Run qemu with installation arguments
 qemu-system-x86_64 -hda 9front.qcow2.img -cdrom ./9front.iso -boot d -vga std -m 768
