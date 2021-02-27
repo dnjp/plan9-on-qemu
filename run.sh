b46663dfd16637f44ededb8bad4474725c831bcb
Daniel <danieljamespost@posteo.net>
2021-02-27 15:39:56 -0500

    working on plan9 vm config

--- run.sh
+++ run.sh
@@ -1,83 +1,104 @@
 #!/bin/sh
 
-function install() {
+img_size=50G
+mem=4G
+iso_file='9front.iso'
+out_file='9front.iso.gz'
+image_name='9front.qcow2.img'
+image_url='http://9front.org/iso/9front-8013.d9e940a768d1.amd64.iso.gz'
+unpack_cmd='gunzip'
 
-	echo -n 'Would you like to install 9front or 9legacy distribution? [9front, 9legacy, 9ants] '
-	read distro
+install() {
+	# download iso
+	if [ ! -f "$out_file" ]; then
+		echo 'downloading iso...'
+		curl -L $image_url -o $out_file 
+	fi
 
+	# unpack iso
+	if [ ! -f "$iso_file" ]; then
+		echo 'unpacking iso...'
+		eval $unpack_cmd $out_file && \
+		stat $iso_file 
+	fi
+
+	# create qemu image
+	if [ ! -f "$image_name" ]; then
+		echo "creating a $img_size qemu image..."
+		qemu-img create -f qcow2 $image_name $img_size
+	fi
 	
-	echo -n 'Specify in GB how large of an image should be created (ex. 30G): '
-	read img_size
-
-	iso_file=''
-	image_name=''
-
-	case $distro in
-		9front)
-			# download iso to 9front.iso
-			curl -L http://9front.org/iso/9front-7781.38dcaeaa222c.amd64.iso.gz -o plan9.iso.gz
-			gunzip 9front.iso.gz
-			stat 9front.iso
-			iso_file='plan9.iso'
-			image_name='9front.qcow2.img'
-			;;
-		9legacy)
-			curl -L http://9legacy.org/download/9legacy.iso.bz2 -o 9legacy.iso.bz2
-			bunzip2 9legacy.iso.bz2
-			stat 9legacy.iso
-			iso_file='9legacy.iso'
-			image_name='9legacy.qcow2.img'
-			;;
-		9ants)
-			curl -L http://files.9gridchan.org/9ants5.64.iso.gz -o 9ants.iso.gz
-			gunzip 9ants.iso.gz
-			stat 9ants.iso
-			iso_file='9ants.iso'
-			image_name='9ants.qcow2.img'
-			;;
-	esac
-
-	qemu-img create -f qcow2 $image_name $img_size
-
-	# Run qemu with installation arguments
-	qemu-system-x86_64 -hda $image_name -cdrom $iso_file -boot d -vga std -m 768
+	if [ -f "$image_name" ]; then
+		echo "installing from $iso_file..."
+		qemu-system-x86_64 -hda $image_name -cdrom $iso_file -boot d -vga std -m 768
+	fi
 }
 
-function run() {
-
-	echo -n 'Do you want to boot 9front or 9legacy? [9front, 9legacy, 9ants] '
-	read opt
-
-	image_name=''
-
-	case $opt in
-		9front)
-			image_name='9front.qcow2.img'
-			;;
-		9legacy)
-			image_name='9legacy.qcow2.img'
-			;;
-		9ants)
-			image_name='9ants.qcow2.img'
-			;;
-	esac
-
-	echo -n 'How much memory in GB should be used for this machine? (ex. 4G) '
-	read mem
-
+run() {
 	# -m option configures memory. feel free to change to match your preferences
-	qemu-system-x86_64 -m $mem $image_name
+	# qemu-system-x86_64 -m $mem $image_name
+	
+	
+#	qemu-system-x86_64 -cpu host -enable-kvm -m 1024  \
+#		-net nic,model=virtio,macaddr=52:54:00:00:EE:03 \
+#		-netdev user,hostfwd=tcp::17019-:17019,hostfwd=tcp::17020-:17020,\		
+#		hostfwd=tcp::12567-:567 -device virtio-scsi-pci,id=scsi  \
+#		-drive if=none,id=vd0,file=9front.qcow2.img -device scsi-hd,drive=vd0	
+
+		
+	# works
+	# qemu-system-x86_64 -m $mem -hda $image_name -net nic \
+    		# -net user,hostfwd=tcp:127.0.0.1:17567-:567,hostfwd=tcp:127.0.0.1:17010-:17010
+		# -net vde
+
+
+	# qemu-system-x86_64 \
+	    # -cpu host -enable-kvm -m $mem \
+	    # -netdev tap,id=eth,ifname=tap0,script=no,downscript=no \
+	    # -device e1000,netdev=eth,mac=52:54:00:00:EE:03 \
+	    # -device virtio-scsi-pci,id=scsi -drive \
+	    # if=none,id=vd0,file=$image_name \
+	    # -device scsi-hd,drive=vd0
+
+
+	SDL_VIDEO_X11_DGAMOUSE=0 
+
+#use kvm and forward cpu features to guest system
+# "tap0" created on host, connected to vm
+# specify a processor architecture to emulate. 
+# specify memory
+# You need at least one virtio-scsi-controller and for each block device a -drive and -device scsi-hd pair.
+
+	# qemu-system-x86_64 -m $mem -hda $image_name -net nic \
+		# -net user,hostfwd=tcp:127.0.0.1:17567-:567,hostfwd=tcp:127.0.0.1:17019-:17019
+
+	# this works
+	# qemu-system-x86_64 \
+		# -cpu host \
+		# -enable-kvm \
+		# -m $mem \
+		# -netdev tap,id=eth,ifname=tap0,script=no,downscript=no \
+		# -device e1000,netdev=eth,mac=52:54:00:00:EE:03 \
+		# -device virtio-scsi-pci,id=scsi \
+		# -drive if=none,id=vd0,file=$image_name \
+		# -device scsi-hd,drive=vd0 
+		
+	qemu-system-x86_64 -cpu host -enable-kvm -m $mem \
+		-net nic,model=virtio,macaddr=52:54:00:00:EE:03 \
+		-net user,hostfwd=tcp::17019-:17019 -net vde -device virtio-scsi-pci,id=scsi \
+		-soundhw sb16 -usb -drive if=none,id=vd0,file=$image_name \
+		-device scsi-hd,drive=vd0		
 }
 
-
-echo -n 'Would you like to install or run Plan 9? [run, install] '
-read action
+action=""
+if [ -z "$1" ]; then
+	echo -n 'Would you like to install or run Plan 9? [run, install] '
+	read action
+else
+	action="$1"
+fi
 
 case $action in
-	install)
-		install
-		;;
-	run)
-		run
-		;;
+	install) install;;
+	run) run;;
 esac
